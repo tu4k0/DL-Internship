92f1503c858544da8024df595da6bb9745db743b
tu4k0 <kirya.mlievskij.00@gmail.com>
2023-02-15 14:20:33 +0200

    Fix version issues

--- Task1-version2/main.py
+++ Task1-version2/main.py
@@ -91,29 +91,25 @@ class BtcBlockchain(BaseBlockchain):
             raise Exception('Node Url invalid')
 
     def createVersionMessage(self, node):
-        version = struct.pack("i", 70016)
+        version = struct.pack("i", 70015)
         services = struct.pack("Q", 0)
         timestamp = struct.pack("q", int(time.time()))
-        add_recv_services = struct.pack("Q", 0)
-        add_recv_ip = struct.pack(">16s", bytes(node, 'utf-8'))
-        add_recv_port = struct.pack(">H", 8333)
-        add_trans_services = struct.pack("Q", 0)
-        add_trans_ip = struct.pack(">16s", bytes("127.0.0.1", 'utf-8'))
-        add_trans_port = struct.pack(">H", 8333)
+
+        add_recv = struct.pack("Q", 0)
+        add_recv += struct.pack(">16s", bytes(node, 'utf-8'))
+        add_recv += struct.pack(">H", 8333)
+
+        add_from = struct.pack("Q", 0)
+        add_from += struct.pack(">16s", bytes("127.0.0.1", 'utf-8'))
+        add_from += struct.pack(">H", 8333)
+
         nonce = struct.pack("Q", random.getrandbits(64))
-        user_agent_bytes = struct.pack("B", 0)
-        starting_height = struct.pack("i", 0)
-        relay = struct.pack("?", False)
+        user_agent = struct.pack("B", 0)
+        height = struct.pack("i", 0)
 
-        payload = version + services + timestamp + add_recv_services + add_recv_ip + add_recv_port + add_trans_services + add_trans_ip + add_trans_port + nonce + user_agent_bytes + starting_height + relay
+        payload = version + services + timestamp + add_recv + add_from + nonce + user_agent + height
 
-        magic = bytes.fromhex("F9BEB4D9")
-
-        command = b"version" + 5 * b"\00"
-        length = struct.pack("I", len(payload))
-        checksum = hashlib.sha256(hashlib.sha256(payload).digest()).digest()[:4]
-
-        return magic + command + length + checksum + payload
+        return payload
 
     def createVerackMessage(self):
         return bytearray.fromhex("f9beb4d976657261636b000000000000000000005df6e0e2")
@@ -124,8 +120,7 @@ class BtcBlockchain(BaseBlockchain):
         recv_length = struct.unpack("I", recv_message[16:20])
         recv_checksum = recv_message[20:24]
         recv_payload = recv_message[24:]
-        recv_version = struct.unpack("i", recv_payload[:4])
-        return (recv_magic, recv_command, recv_length, recv_checksum, recv_payload, recv_version)
+        return (recv_magic, recv_command, recv_length, recv_checksum, recv_payload)
 
     def getData(self, block_hash):
         count = 1
@@ -134,16 +129,19 @@ class BtcBlockchain(BaseBlockchain):
         payload = struct.pack('<bb32s', count, type, hash)
         return payload
 
-    def create_message(self, magic, command, payload):
-        checksum = hashlib.sha256(hashlib.sha256(payload).digest()).digest()[0:4]
-        return (struct.pack('L12sL4s', magic, command.encode(), len(payload), checksum) + payload)
+    def makeMessage(self, command, payload):
+        magic = bytes.fromhex('F9BEB4D9')
+        command = bytes(command, 'utf-8') + (12 - len(command)) * b"\00"
+        length = struct.pack("I", len(payload))
+        checksum = hashlib.sha256(hashlib.sha256(payload).digest()).digest()[:4]
+        return magic + command + length + checksum + payload
+
 
 if __name__ == '__main__':
     print('Service for manual node connection to Blockchain networks')
     blockchainName = 'BTC'
     if blockchainName == 'BTC':
         BTC = BtcBlockchain()
-        print(BTC.getIp())
         print(BTC.setSocket())
         print('BTC socket info: ', BTC.socket)
         print(BTC.getNodeAddresses())
@@ -152,18 +150,23 @@ if __name__ == '__main__':
         connection = BTC.connectNode(node=node, port=int(port))
         if connection:
             print('Connection status: True')
-        verMessage = BTC.createVersionMessage(node)
-        print('Send -Version- message to node')
-        BTC.socket.send(verMessage)
-        time.sleep(1)
-        print('Receive -Version- message from node')
-        encoded_values = BTC.encodeReceivedMessage(BTC.socket.recv(8192))
-        print("Version ping result: ", encoded_values)
-        print('Verack: ')
-        verack = BTC.createVerackMessage()
-        send = BTC.socket.send(verack)
-        receive = BTC.socket.recv(8192)
-        print(binascii.hexlify(receive))
+        print('Version message: ')
+        BTC.socket.send(BTC.makeMessage("version", BTC.createVersionMessage(node)))
+        print(BTC.encodeReceivedMessage(BTC.socket.recv(1024))[1])
+        print('Verack message: ')
+        print(BTC.socket.recv(1024))
+        # verMessage = BTC.createVersionMessage(node)
+        # print('Send -Version- message to node')
+        # BTC.socket.send(verMessage)
+        # time.sleep(1)
+        # print('Receive -Version- message from node')
+        # encoded_values = BTC.encodeReceivedMessage(BTC.socket.recv(8192))
+        # print("Version ping result: ", encoded_values)
+        # print('Verack: ')
+        # verack = BTC.createVerackMessage()
+        # send = BTC.socket.send(verack)
+        # receive = BTC.socket.recv(8192)
+        # print(binascii.hexlify(receive))
         # print('GetData: ')
         # data = BTC.getData('1dbd981fe6985776b644b173a4d0385ddc1aa2a829688d1e0000000000000000')
         # getdata_message = BTC.create_message(0xd9b4bef9, 'getdata', data)
