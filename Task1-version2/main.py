d76491ed4cc7156b6b5809ee3317f6938da0409e
tu4k0 <kirya.mlievskij.00@gmail.com>
2023-02-15 10:29:16 +0200

    Update class BtcBlockchain

--- Task1-version2/main.py
+++ Task1-version2/main.py
@@ -1,9 +1,13 @@
+import binascii
 import hashlib
 import random
 import socket
 import struct
 import time
 from abc import abstractmethod, ABC
+
+import requests
+import re
 
 
 class BaseBlockchain(ABC):
@@ -13,7 +17,7 @@ class BaseBlockchain(ABC):
         pass
 
     @abstractmethod
-    def getData(self) -> dict:
+    def getData(self, tx_id):
         pass
 
 
@@ -28,34 +32,52 @@ class EthBlockchain(BaseBlockchain):
 
 class BtcBlockchain(BaseBlockchain):
     node: str
-    sock: socket
+    PORT: int
+    socket: socket
+    dns_seeds = [
+        ("seed.bitcoin.sipa.be", 8333),
+        ("dnsseed.bluematt.me", 8333),
+        ("dnsseed.bitcoin.dashjr.org", 8333),
+        ("seed.bitcoinstats.com", 8333),
+        ("seed.bitnodes.io", 8333),
+        ("bitseed.xf2.org", 8333),
+    ]
 
     def __init__(self):
         super().__init__()
 
     def setSocket(self):
-        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        if str(self.sock.type) == 'SocketKind.SOCK_STREAM':
-            return self.sock
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        if str(self.socket.type) == 'SocketKind.SOCK_STREAM':
+            return self.socket
         else:
             return 0
 
-    def getNodeAddresses(self):
-        dns_seeds = [
-            ("seed.bitcoin.sipa.be", 8333),
-            ("dnsseed.bluematt.me", 8333),
-            ("dnsseed.bitcoin.dashjr.org", 8333),
-            ("seed.bitcoinstats.com", 8333),
-            ("seed.bitnodes.io", 8333),
-            ("bitseed.xf2.org", 8333),
-        ]
+    def getIp(self):
+        ip = requests.get('https://checkip.amazonaws.com').text.strip()
+        return ip
 
+    def setNode(self):
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.bind(('', 8333))
+
+    def getConnections(self):
+        self.socket.listen(5)
+        while True:
+            user, addr = self.socket.accept()
+            return user, addr
+
+    def getNodeAddresses(self) -> list:
         found_peers = []
         try:
-            for (ip_address, port) in dns_seeds:
-                for info in socket.getaddrinfo(ip_address, port,
-                                               socket.AF_INET, socket.SOCK_STREAM,
-                                               socket.IPPROTO_TCP):
+            for (ip_address, port) in self.dns_seeds:
+                for info in socket.getaddrinfo(
+                        ip_address,
+                        port,
+                        socket.AF_INET,
+                        socket.SOCK_STREAM,
+                        socket.IPPROTO_TCP
+                ):
                     found_peers.append((info[4][0], info[4][1]))
         except Exception:
             return found_peers
@@ -63,13 +85,13 @@ class BtcBlockchain(BaseBlockchain):
     def connectNode(self, node, port):
         try:
             print("Trying to connect to BTC node: ", node)
-            self.sock.connect((node, port))
+            self.socket.connect((node, port))
             return node
         except Exception:
             raise Exception('Node Url invalid')
 
     def createVersionMessage(self, node):
-        version = struct.pack("i", 70015)
+        version = struct.pack("i", 70016)
         services = struct.pack("Q", 0)
         timestamp = struct.pack("q", int(time.time()))
         add_recv_services = struct.pack("Q", 0)
@@ -80,11 +102,10 @@ class BtcBlockchain(BaseBlockchain):
         add_trans_port = struct.pack(">H", 8333)
         nonce = struct.pack("Q", random.getrandbits(64))
         user_agent_bytes = struct.pack("B", 0)
-        starting_height = struct.pack("i", 525453)
+        starting_height = struct.pack("i", 0)
         relay = struct.pack("?", False)
 
-        payload = version + services + timestamp + add_recv_services + add_recv_ip + add_recv_port + \
-                  add_trans_services + add_trans_ip + add_trans_port + nonce + user_agent_bytes + starting_height + relay
+        payload = version + services + timestamp + add_recv_services + add_recv_ip + add_recv_port + add_trans_services + add_trans_ip + add_trans_port + nonce + user_agent_bytes + starting_height + relay
 
         magic = bytes.fromhex("F9BEB4D9")
 
@@ -94,7 +115,10 @@ class BtcBlockchain(BaseBlockchain):
 
         return magic + command + length + checksum + payload
 
-    def encode_received_message(self, recv_message):
+    def createVerackMessage(self):
+        return bytearray.fromhex("f9beb4d976657261636b000000000000000000005df6e0e2")
+
+    def encodeReceivedMessage(self, recv_message):
         recv_magic = recv_message[:4].hex()
         recv_command = recv_message[4:16]
         recv_length = struct.unpack("I", recv_message[16:20])
@@ -103,19 +127,26 @@ class BtcBlockchain(BaseBlockchain):
         recv_version = struct.unpack("i", recv_payload[:4])
         return (recv_magic, recv_command, recv_length, recv_checksum, recv_payload, recv_version)
 
-    def getData(self):
-        return self
+    def getData(self, block_hash):
+        count = 1
+        type = 2
+        hash = bytearray.fromhex(block_hash)
+        payload = struct.pack('<bb32s', count, type, hash)
+        return payload
 
+    def create_message(self, magic, command, payload):
+        checksum = hashlib.sha256(hashlib.sha256(payload).digest()).digest()[0:4]
+        return (struct.pack('L12sL4s', magic, command.encode(), len(payload), checksum) + payload)
 
 if __name__ == '__main__':
     print('Service for manual node connection to Blockchain networks')
-    blockchainName = input('Enter Blockchain name: ')
+    blockchainName = 'BTC'
     if blockchainName == 'BTC':
         BTC = BtcBlockchain()
-        BTC.setSocket()
-        socketInfo = str(BTC.setSocket())
-        print('BTC socket info: ', socketInfo)
-        # print(BTC.getNodeAddresses())
+        print(BTC.getIp())
+        print(BTC.setSocket())
+        print('BTC socket info: ', BTC.socket)
+        print(BTC.getNodeAddresses())
         node = input('Enter node URL: ')
         port = input('Enter port: ')
         connection = BTC.connectNode(node=node, port=int(port))
@@ -123,7 +154,20 @@ if __name__ == '__main__':
             print('Connection status: True')
         verMessage = BTC.createVersionMessage(node)
         print('Send -Version- message to node')
-        BTC.sock.send(verMessage)
-        time.sleep(5)
-        encoded_values = BTC.encode_received_message(BTC.sock.recv(8192))
-        print("Version ping result: ", encoded_values[-1])
+        BTC.socket.send(verMessage)
+        time.sleep(1)
+        print('Receive -Version- message from node')
+        encoded_values = BTC.encodeReceivedMessage(BTC.socket.recv(8192))
+        print("Version ping result: ", encoded_values)
+        print('Verack: ')
+        verack = BTC.createVerackMessage()
+        send = BTC.socket.send(verack)
+        receive = BTC.socket.recv(8192)
+        print(binascii.hexlify(receive))
+        # print('GetData: ')
+        # data = BTC.getData('1dbd981fe6985776b644b173a4d0385ddc1aa2a829688d1e0000000000000000')
+        # getdata_message = BTC.create_message(0xd9b4bef9, 'getdata', data)
+        # sendy = BTC.socket.send(getdata_message)
+        # rec = BTC.socket.recv(1024)
+        # print(sendy)
+        # print(binascii.hexlify(rec))
