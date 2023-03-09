e59c9092da41a166457820eec40909774bd02509
tu4k0 <kirya.mlievskij.00@gmail.com>
2023-03-09 12:04:41 +0200

    Create eth_config data

---
+++ Task1-version2/application/eth_blockchain/eth_config.py
@@ -0,0 +1,11 @@
+eth_mainnet_port = 8545
+
+host = "192.168.0.100"
+
+ip_link = 'https://checkip.amazonaws.com'
+
+json_version = '2.0'
+
+json_id = '0'
+
+tx_hash_example = '0xb5c8bd9430b6cc87a0e2fe110ece6bf527fa4f170a4bc8cd032f768fc5219838'
