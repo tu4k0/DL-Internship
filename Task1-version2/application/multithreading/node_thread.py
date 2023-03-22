eeb40529cedbb40decec1adf7ea3326ce445eb0f
tu4k0 <kirya.mlievskij.00@gmail.com>
2023-03-22 10:34:47 +0200

    Refactor code

--- Task1-version2/application/multithreading/node_thread.py
+++ Task1-version2/application/multithreading/node_thread.py
@@ -1,20 +1,56 @@
 import threading
 
+from application.btc_blockchain.btc_blockchain import BtcBlockchain
+from application.base_blockchain.base_blockchain import BaseBlockchain
 
-class NodeThread:
 
-    def __init__(self, name, counter):
-        threading.Thread.__init__(self)
-        self.threadID = counter
-        self.name = name
-        self.counter = counter
+class NodeThread(threading.Thread):
+    threads = []
+    sockets = []
+    requests = []
+    responses = []
+    lock = threading.Lock()
 
-    def set_threads(self, node_number):
-        threads = []
-        for i in range(node_number):
-            t = threading.Thread(target=please_sleep, args=[i])
-            t.start()
-            threads.append(t)
+    def __init__(self):
+        super().__init__()
+
+    def set_thread(self, nodes, blockchain):
+        for key, value in nodes.items():
+            blockchain.set_socket()
+            connection = blockchain.connect_node(ip_address=key, port=value)
+            self.sockets.append(connection)
+            thread = threading.Thread(target=self.set_threads_function, args=(blockchain, key,))
+            thread.start()
+            print('\nNode: ', f'{key}:{value}')
+            self.threads.append(thread)
+            thread.join()
+
+    def set_sockets(self, nodes):
+        for key, value in nodes.items():
+            sock = BtcBlockchain()
+            socket = sock.set_socket()
+            self.sockets.append(socket)
+            print(sock.connect_node(ip_address=key, port=value))
+
+    def print_sockets(self):
+        return self.sockets
+
+    def set_threads_function(self, blockchain, ip_address):
+        self.lock.acquire()
+        request = blockchain.make_message("version", blockchain.create_version_message(ip_address))
+        blockchain.send_message(request)
+        self.requests.append(request)
+        response = blockchain.receive_message()
+        self.responses.append(response)
+        blockchain.print_response("version", request, response)
+        self.lock.release()
+
+    def stop_threads(self, nodes):
+        for i in range(len(nodes)):
+            self.threads[i].join()
 
     def get_active_threads(self):
-        return threading.active_count()
+        return self.threads
+
+    def get_active_connections(self):
+        return len(self.sockets)
