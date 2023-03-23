d3a24bffe6ed60b2ff6e8f4ebcfff7c6ff76c1b7
tu4k0 <kirya.mlievskij.00@gmail.com>
2023-03-23 22:55:22 +0200

    Refactor code

--- Task1-version2/application/multithreading/node_thread.py
+++ Task1-version2/application/multithreading/node_thread.py
@@ -1,6 +1,8 @@
 import threading
 
 from application.btc_blockchain.btc_blockchain import BtcBlockchain
+from application.statistic.statistic import Statistic
+from application.btc_blockchain.btc_config import *
 from application.base_blockchain.base_blockchain import BaseBlockchain
 
 
@@ -9,48 +11,32 @@ class NodeThread(threading.Thread):
     sockets = []
     requests = []
     responses = []
-    lock = threading.Lock()
+    lock = threading.RLock()
 
     def __init__(self):
         super().__init__()
 
-    def set_thread(self, nodes, blockchain):
-        for key, value in nodes.items():
-            blockchain.set_socket()
-            connection = blockchain.connect_node(ip_address=key, port=value)
-            self.sockets.append(connection)
-            thread = threading.Thread(target=self.set_threads_function, args=(blockchain, key,))
-            thread.start()
-            print('\nNode: ', f'{key}:{value}')
-            self.threads.append(thread)
-            thread.join()
+    # def set_thread(self, nodes, blockchain):
+    #     for key, value in nodes.items():
+    #         blockchain.set_socket()
+    #         connection = blockchain.connect_node(ip_address=key, port=value)
+    #         thread = threading.Thread(target=self.collect_blockchain_info, args=(blockchain, key,))
+    #         thread.start()
+    #         print('\nNode: ', f'{key}:{value}')
+    #         self.threads.append(thread)
+    #         thread.join()
 
-    def set_sockets(self, nodes):
-        for key, value in nodes.items():
-            sock = BtcBlockchain()
-            socket = sock.set_socket()
-            self.sockets.append(socket)
-            print(sock.connect_node(ip_address=key, port=value))
+    def set_node_socket(self, blockchain, ip_address, port):
+        if type(blockchain) == BtcBlockchain:
+            node_socket = BtcBlockchain()
+            node_socket.set_socket()
+            node_socket.connect_node(ip_address, port)
+            return node_socket
 
-    def print_sockets(self):
-        return self.sockets
+    def collect_blockchain_info(self, blockchain, ip_address, port, statistic):
+        with self.lock:
+            node = self.set_node_socket(blockchain, ip_address, port)
+            statistic.btc_blockchains_objects.append(node)
+            node.execute_message(command_name='version', payload=ip_address)
+            node.execute_message(command_name='verack')
 
-    def set_threads_function(self, blockchain, ip_address):
-        self.lock.acquire()
-        request = blockchain.make_message("version", blockchain.create_version_message(ip_address))
-        blockchain.send_message(request)
-        self.requests.append(request)
-        response = blockchain.receive_message()
-        self.responses.append(response)
-        blockchain.print_response("version", request, response)
-        self.lock.release()
-
-    def stop_threads(self, nodes):
-        for i in range(len(nodes)):
-            self.threads[i].join()
-
-    def get_active_threads(self):
-        return self.threads
-
-    def get_active_connections(self):
-        return len(self.sockets)
