585dceb758c0bf6eeb7c370c574155788d7c94c9
tu4k0 <kirya.mlievskij.00@gmail.com>
2023-03-23 22:55:03 +0200

    Update statistic collecting

--- Task1-version2/application/statistic/statistic.py
+++ Task1-version2/application/statistic/statistic.py
@@ -1,26 +1,38 @@
-import threading
+from application.btc_blockchain.btc_blockchain import BtcBlockchain
 
+import threading
 
 from application.btc_blockchain.btc_blockchain import BtcBlockchain
 
+
 class Statistic:
-    thread: threading.Thread
-    connections: list = []
-    requests: list
+    requests: int
     responses: list
+    connections: list
+    threads: list
+    btc_blockchains_objects: list
+    nodes: dict
+    etc_blockchain_objects: list
 
     def __init__(self):
-        pass
+        self.requests = 0
+        self.responses = []
+        self.connections = []
+        self.threads = []
+        self.nodes = {}
+        self.btc_blockchains_objects = []
 
-    def collect_statistic(self):
-        pass
+    def show_blockchain_statistic(self):
+        for object in self.btc_blockchains_objects:
+            print(object.requests)
 
-    def set_active_connections(self):
-        pass
-
-    def get_active_connections(self):
-        return len(self.connections)
-
-    def get_requests(self, blockchain):
-        return len(blockchain.requests)
+    def print_nodes(self):
+        print('Nodes: ')
+        if self.nodes:
+            node_counter = 1
+            for key, value in self.nodes.items():
+                print('Node', node_counter, '  ', f'{key},', value)
+                node_counter += 1
+        else:
+            print('Failed to get node peers! Try again')
 
