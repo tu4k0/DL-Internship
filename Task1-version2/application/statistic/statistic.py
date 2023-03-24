c2c17841455c5431ab21de8c872ee6848db6c343
tu4k0 <kirya.mlievskij.00@gmail.com>
2023-03-24 12:57:22 +0200

    Update statistic collection

--- Task1-version2/application/statistic/statistic.py
+++ Task1-version2/application/statistic/statistic.py
@@ -1,38 +1,54 @@
-from application.btc_blockchain.btc_blockchain import BtcBlockchain
-
-import threading
-
-from application.btc_blockchain.btc_blockchain import BtcBlockchain
+import datetime
 
 
 class Statistic:
     requests: int
-    responses: list
+    responses: int
     connections: list
     threads: list
     btc_blockchains_objects: list
     nodes: dict
     etc_blockchain_objects: list
+    update_time: datetime.datetime
 
     def __init__(self):
         self.requests = 0
-        self.responses = []
+        self.responses = 0
         self.connections = []
         self.threads = []
         self.nodes = {}
         self.btc_blockchains_objects = []
 
     def show_blockchain_statistic(self):
+        self.update_time = datetime.datetime.now()
+        print('\nStatistics\n')
+        print(f"Active connections: {len(self.connections)}\n")
+        self.get_nodes_list()
+        node_counter = 1
         for object in self.btc_blockchains_objects:
-            print(object.requests)
+            print('\n')
+            print('Node', node_counter, '  ', f'{object.ip_address},', object.port)
+            print(f'{self.threads[node_counter - 1].name}: daemon-{self.threads[node_counter - 1].daemon}')
+            node_counter += 1
+            print(f"Commands {object.commands}:")
+            print("Requests:")
+            for request in object.requests:
+                self.requests += 1
+                print(request)
+            print("Responses:")
+            for response in object.responses:
+                self.responses += 1
+                print(response)
+        print(f"\nTotal number of sent messages: {self.requests}")
+        print(f"\nTotal number of received messages: {self.responses}")
+        print(f"\nUpdated at: {self.update_time}")
 
-    def print_nodes(self):
-        print('Nodes: ')
-        if self.nodes:
+    def get_nodes_list(self):
+        print('Connected nodes list: ')
+        if self.btc_blockchains_objects:
             node_counter = 1
-            for key, value in self.nodes.items():
-                print('Node', node_counter, '  ', f'{key},', value)
+            for object in self.btc_blockchains_objects:
+                print('Node', node_counter, '  ', f'{object.ip_address},', object.port)
                 node_counter += 1
         else:
             print('Failed to get node peers! Try again')
-
