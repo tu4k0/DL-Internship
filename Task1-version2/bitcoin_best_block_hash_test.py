2a677834631d9e48e34b61fe8afcace4820a0dcd
tu4k0 <kirya.mlievskij.00@gmail.com>
2023-03-31 18:37:11 +0300

    Update bitcoin_best_block_hash_test.py (add create_sub_version() and create_network_address(), fix delay bugs, remove loops)

--- Task1-version2/bitcoin_best_block_hash_test.py
+++ Task1-version2/bitcoin_best_block_hash_test.py
@@ -1,3 +1,4 @@
+import binascii
 import hashlib
 import random
 import socket
@@ -6,70 +7,57 @@ import time
 
 from application.btc_blockchain.btc_config import *
 
+constant = {'magic_value': 0xd9b4bef9,
+             'peer_ip_address': '100.36.127.250',
+             'peer_tcp_port': 8333,
+             'buffer_size': 4096}
 
-# def make_message(command, payload):
-#     magic = bytes.fromhex(btc_magic)
-#     command = bytes(command, 'utf-8') + (12 - len(command)) * b"\00"
-#     length = struct.pack("I", len(payload))
-#     checksum = hashlib.sha256(hashlib.sha256(payload).digest()).digest()[:4]
-#
-#     return magic + command + length + checksum + payload
-#
-# def create_version_message(node_ip) -> bytes:
-#     version = struct.pack("i", btc_version)
-#     services = struct.pack("Q", 0)
-#     timestamp = struct.pack("q", int(time.time()))
-#     add_recv = struct.pack("Q", 0)
-#     add_recv += struct.pack(">16s", bytes(node_ip, 'utf-8'))
-#     add_recv += struct.pack(">H", 8333)
-#     add_from = struct.pack("Q", 0)
-#     add_from += struct.pack(">16s", bytes(host, 'utf-8'))
-#     add_from += struct.pack(">H", 8333)
-#     nonce = struct.pack("Q", random.getrandbits(64))
-#     user_agent = struct.pack("B", 0)
-#     height = struct.pack("i", 0)
-#     payload = version + services + timestamp + add_recv + add_from + nonce + user_agent + height
-#
-#     return payload
 
-def create_getdata_message():
-    block_hash = '0000000000000000000549a764cc6f1ba850fe694249b87b41c54113e754d1c6'
-    count = struct.pack("<B", 1)
-    type = struct.pack("<L", 2)  # type : MSG_BLOCK
-    hash = struct.pack(">32s", block_hash.encode())  # разобраться с этим или спросить у Богдана
-    inventory = type + hash
+def create_sub_version():
+    sub_version = "/Satoshi:0.24.1/"
 
-    payload = count + inventory
+    return b'\x0F' + sub_version.encode()
+
+
+def create_network_address(ip_address, port):
+    network_address = struct.pack('>8s16sH', b'\x01',
+                                  bytearray.fromhex("00000000000000000000ffff") + socket.inet_aton(ip_address), port)
+
+    return network_address
+
+
+def create_getdata_payload():
+    count = 0
+    type = 2
+    hash = bytearray.fromhex('0000000000000000000000000000000000000000000000000000000000000000')
+    payload = struct.pack('<bb32s', count, type, hash)
+
     return payload
 
 
-def create_version_message():
-    version = struct.pack("i", 70015)
-    services = struct.pack("Q", 0)
-    timestamp = struct.pack("q", int(time.time()))
-    addr_recv_services = struct.pack("Q", 0)
-    addr_recv_ip = struct.pack(">16s", b"127.0.0.1")
-    addr_recv_port = struct.pack(">H", 8333)
-    addr_from_services = struct.pack("Q", 0)
-    addr_from_ip = struct.pack(">16s", b"127.0.0.1")
-    addr_from_port = struct.pack(">H", 8333)
-    nonce = struct.pack("Q", random.getrandbits(64))
-    user_agent_bytes = struct.pack("B", 0)
-    start_height = struct.pack("i", 783053)
-    relay = struct.pack("?", False)
-
-    payload = version + services + timestamp + addr_recv_services + addr_recv_ip + addr_recv_port + addr_from_services + addr_from_ip + addr_from_port + nonce + user_agent_bytes + start_height + relay
+def create_version_payload(peer_ip_address):
+    version = 70015
+    services = 1
+    timestamp = int(time.time())
+    addr_local = create_network_address("127.0.0.1", 8333)
+    addr_peer = create_network_address(peer_ip_address, 8333)
+    nonce = random.getrandbits(64)
+    start_height = 0
+    payload = struct.pack('<LQQ26s26sQ16sL', version, services, timestamp, addr_peer,
+                          addr_local, nonce, create_sub_version(), start_height)
     return payload
 
 
-def create_verack_message():
+def create_verack_payload():
     payload = bytearray.fromhex("f9beb4d976657261636b000000000000000000005df6e0e2")
 
     return payload
 
-def create_getaddr_message():
+
+def create_getaddr_payload():
     payload = b""
     return payload
+
 
 def create_message(command, payload):
     magic = bytes.fromhex("F9BEB4D9")
@@ -83,28 +71,43 @@ def create_message(command, payload):
 
     return message
 
-def getdata_message():
-    count = 1
-    type = 1
-    hash = bytearray.fromhex('0000000000000000000000000000000000000000000000000000000000000000')
-    payload = struct.pack('<bb32s', count, type, hash)
 
-    return payload
+# def getdata_message():
+#     block_hash = '0000000000000000000549a764cc6f1ba850fe694249b87b41c54113e754d1c6'
+#     count = struct.pack("<B", 1)
+#     type = struct.pack("<L", 2)  # type : MSG_BLOCK
+#     hash = struct.pack(">32s", block_hash.encode())  # need fix
+#     inventory = type + hash
+#
+#     payload = count + inventory
+#     return payload
 
-def create_ping_message() -> bytes:
+
+# def print_response(command, request_data, response_data):
+#     print("")
+#     print("Command: " + command)
+#     print("Request:")
+#     print(binascii.hexlify(request_data))
+#     print("Response:")
+#     print(response_data)
+
+
+def create_ping_payload() -> bytes:
     nonce = random.randint(1, 1 ** 32)
     payload = struct.pack('<Q', nonce)
 
     return payload
 
-def create_getheaders_message(start_block_header_hash):
+
+def create_getheaders_payload(start_block_header_hash):
     version = struct.pack("i", 70015)
     hash_count = struct.pack("<b", 1)
     hash_stop = bytes.fromhex("00" * 32)
     payload = version + hash_count + start_block_header_hash + hash_stop
     return payload
 
-def create_getblocks_message():
+
+def create_getblocks_payload():
     version = struct.pack("i", 70015)
     hash_count = struct.pack("<b", 1)
     hash_stop = bytes.fromhex("00000000839a8e6886ab5951d76f411475428afc90947ee320161bbf18eb6048")
@@ -112,42 +115,54 @@ def create_getblocks_message():
     payload = version + hash_count + start_block_header_hash + hash_stop
     return payload
 
-# Connect to a Bitcoin node
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(('100.36.127.250', 8333))
-s.send(create_message("version", create_version_message()))
-receive = s.recv(1024)
-recv = s.recv(1024)
-print('1',receive)
-print('2', recv)
-time.sleep(1)
-s.send(create_message("verack", create_verack_message()))
-time.sleep(1)
-# s.send(create_message("getdata", getdata_message('1dbd981fe6985776b644b173a4d0385ddc1aa2a829688d1e0000000000000000')))
-print('3', s.recv(1024))
-# s.send(create_message("getaddr", create_getaddr_message()))
-# res1 = s.recv(1024)
-# res2 = s.recv(1024)
-# print(res1)
-# time.sleep(7)
-s.send(create_message("getheaders", create_getheaders_message(bytes.fromhex("000000000000000000034ab50a4dea2037325fd160cb0e3bb2e1f7c580517ceb"))))
-response = b""
-while True:
-    data = s.recv(1024)
-    response+=data
-    if len(data) < 1024:
-        break
-print('End while')
-print(response)
-time.sleep(3)
-s.send(create_message("getdata", getdata_message()))
-for i in range(1,41):
-    print(s.recv(4096))
-print('stop')
-time.sleep(3)
-print(s.recv(80))
-# s.send(create_message("getheaders", create_getheaders_message(bytes.fromhex("000000000000000000034ab50a4dea2037325fd160cb0e3bb2e1f7c580517ceb"))))
-# time.sleep(3)
-# data = s.recv(1024)
-# print(data)
+
+if __name__ == '__main__':
+    start_time = time.time()
+
+    # Create Messages
+    version_payload = create_version_payload(constant['peer_ip_address'])
+    version_message = create_message('version', version_payload)
+    verack_message = create_verack_payload()
+    getdata_payload = create_getdata_payload()
+    getdata_message = create_message('getdata', getdata_payload)
+
+    # Establish Node TCP Connection
+    node = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    node.connect((constant['peer_ip_address'], constant['peer_tcp_port']))
+
+    # Send message "version"
+    node.send(version_message)
+    response_data = node.recv(constant['buffer_size'])
+
+    # Send message "verack"
+    node.send(verack_message)
+    response_data = node.recv(constant['buffer_size'])
+
+    # Send message "getdata"
+    node.send(getdata_message)
+    node.recv(constant['buffer_size'])
+    response_data = node.recv(constant['buffer_size'])
+    result = str(response_data)
+
+    index = result.find("getheaders")
+    if index == -1:
+        getheaders = node.recv(constant['buffer_size'])
+        res = str(getheaders)
+        index = res.find("getheaders")
+        Block_hash = binascii.hexlify(getheaders)[index+40:index+104]
+        hash = Block_hash.decode("utf-8")
+        response = bytearray.fromhex(hash)
+        response.reverse()
+        print('Bitcoin best block hash: ', response.hex())
+    else:
+        Block_hash = binascii.hexlify((response_data))[140:204]
+        hash = Block_hash.decode("utf-8")
+        response = bytearray.fromhex(hash)
+        response.reverse()
+        print('Bitcoin best block hash: ', response.hex())
+
+    print('Retreiving block hash data execution time: ', time.time()-start_time)
+
+    # Disconnect from node and Close the TCP connection
+    node.close()
 
