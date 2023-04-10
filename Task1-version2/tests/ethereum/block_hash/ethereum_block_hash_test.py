759b25538f92d958c33cf9fb13d66b6c30cdef04
tu4k0 <kirya.mlievskij.00@gmail.com>
2023-04-10 20:11:25 +0300

    Refactor methods naming

--- Task1-version2/tests/ethereum/block_hash/ethereum_block_hash_test.py
+++ Task1-version2/tests/ethereum/block_hash/ethereum_block_hash_test.py
@@ -11,7 +11,7 @@ constant = {'peer_ip_address': '183.136.220.19',
              'buffer_size': 4096}
 
 
-def make_message(message):
+def create_message(message):
     request_method = "POST / HTTP/1.1\r\n"
     host = f"Host: 192.168.0.100\r\n"
     content_type = f"Content-Type: application/json\r\n"
@@ -21,7 +21,7 @@ def make_message(message):
     return message.encode('utf-8')
 
 
-def create_getblock_number_message():
+def create_getblock_number_payload():
     method = 'eth_blockNumber'
     block_number_message = {
         'jsonrpc': '2.0',
@@ -32,7 +32,8 @@ def create_getblock_number_message():
 
     return block_number_message
 
-def create_getblock_hash_message():
+
+def create_getblock_hash_payload():
     method = 'eth_getBlockByNumber'
     block_hash_message = {
         'jsonrpc': '2.0',
@@ -61,10 +62,10 @@ if __name__ == '__main__':
     start_time = time.time()
 
     # Create Messages
-    getblock_payload = create_getblock_number_message()
-    getblock_message = make_message(getblock_payload)
-    getblockhash_payload = create_getblock_hash_message()
-    getblockhash_message = make_message(getblockhash_payload)
+    getblock_payload = create_getblock_number_payload()
+    getblock_message = create_message(getblock_payload)
+    getblockhash_payload = create_getblock_hash_payload()
+    getblockhash_message = create_message(getblockhash_payload)
 
     # Establish Node TCP Connection
     print('Setting connection')
@@ -85,7 +86,7 @@ if __name__ == '__main__':
     #Show statistic
     print('Retreiving block hash data execution time: ', time.time()-start_time)
     print(result)
-    print(decode_response_message(result)['result'])
+    print(int(decode_response_message(result)['result'], 16))
 
     # Disconnect from node and Close the TCP connection
     node.close()
