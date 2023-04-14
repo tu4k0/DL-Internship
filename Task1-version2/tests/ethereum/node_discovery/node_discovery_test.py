dcc7dbffae5cd2488d83ed3c5e00fb2f07aea670
tu4k0 <kirya.mlievskij.00@gmail.com>
2023-04-14 19:17:13 +0300

    Update get_nodes method (add found peers tuple, fix naming, make return statement).

--- Task1-version2/tests/ethereum/node_discovery/node_discovery_test.py
+++ Task1-version2/tests/ethereum/node_discovery/node_discovery_test.py
@@ -1,26 +1,25 @@
 import csv
-import pandas as pd
-from pathlib import Path
 
 nodes_list = 'C:/Users/Admin/Desktop/Tu4k0/DL-Internship/Task1-version2/application/eth_blockchain/ethereum-nodestrackerlist.csv'
 
 
-def get_nodes_list(nodes_list, node_number):
-    with open(nodes_list, 'r') as csv_file:
-        reader = csv.reader(csv_file)
-
-        counter = 0
-
-        for row in reader:
-            if counter == node_number+1:
+def get_nodes(nodes_list, node_number):
+    with open(nodes_list, 'r') as nodes:
+        nodes = csv.reader(nodes)
+        found_peers = dict()
+        search_index = 0
+        for node_info in nodes:
+            if search_index == node_number + 1:
                 break
             else:
-                if counter == 0:
-                    counter += 1
+                if search_index == 0:
+                    search_index += 1
                 else:
-                    print(row[2])
-                    counter += 1
+                    found_peers.update({node_info[2]: 8545})
+                    search_index += 1
+
+    return found_peers
 
 
 if __name__ == '__main__':
-    get_nodes_list(nodes_list, 5)
+    print(get_nodes(nodes_list, 5))
