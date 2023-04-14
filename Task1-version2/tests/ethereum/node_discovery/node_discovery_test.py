70c88e06e3afb801ab013a7e55a1ba6cdfc63ed0
tu4k0 <kirya.mlievskij.00@gmail.com>
2023-04-14 13:31:26 +0300

    Update node discovery through web scrapping (BeautifulSoup)

--- Task1-version2/tests/ethereum/node_discovery/node_discovery_test.py
+++ Task1-version2/tests/ethereum/node_discovery/node_discovery_test.py
@@ -1,17 +1,15 @@
 import requests
+from bs4 import BeautifulSoup
 
-# replace with your node's IP address and port number
-node_url = 'http://135.181.37.236:8545'
+# Making a GET request
+r = requests.get('https://etherscan.io/nodetracker/nodes')
+print("Http Response Code:", r)
 
-# retrieve the total number of peers connected to the node
-payload = {"jsonrpc": "2.0", "id": 1, "method": "net_peerCount", "params": []}
-response = requests.post(node_url, json=payload)
-peer_count = int(response.json()['result'], 16)
-print(peer_count)
+# Parsing the HTML
+soup = BeautifulSoup(r.content, 'html.parser')
+print("HTML content ", soup)
 
-# retrieve information about each connected peer, including IP address
-for i in range(peer_count):
-    payload = {"jsonrpc": "2.0", "id": i+1, "method": "net_peerState", "params": [hex(i)]}
-    response = requests.post(node_url, json=payload)
-    result = response.json()
-    print(result)
+s = soup.find('div', class_='entry-content')
+content = s.find_all('p')
+
+print(content)
