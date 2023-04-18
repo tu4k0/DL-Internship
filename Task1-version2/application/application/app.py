1123509cddd83583ce7cd48391b93fe8ab831857
tu4k0 <kirya.mlievskij.00@gmail.com>
2023-04-18 23:56:50 +0300

    Update class CLI (add argparse arguments)

--- Task1-version2/application/application/app.py
+++ Task1-version2/application/application/app.py
@@ -1,5 +1,6 @@
 import os
 import time
+import sys
 
 from application.btc_blockchain.btc_blockchain import BtcBlockchain
 from application.eth_blockchain.eth_blockchain import EthBlockchain
@@ -12,7 +13,20 @@ class CLI:
     BTC: BtcBlockchain
     ETH: EthBlockchain
     node: str
+    cli_arguments: argparse.ArgumentParser
 
+    def __init__(self):
+        self.cli_arguments = argparse.ArgumentParser(description="Bitcoin/Ethereum P2P network implementation")
+        self.cli_arguments.add_argument("-ip", dest="ip_address", required=True, type=str)
+        self.cli_arguments.add_argument("-port", dest="port", required=True, type=int)
+        self.cli_arguments.add_argument("-num", dest="node_number", default=1, type=int)
+
+    def run(self):
+        print(self.cli_arguments.parse_args())
+
+
+cli = CLI()
+cli.run()
 #     def __get_response_by_command(self, blockchain, message_command: str, request):
 #         blockchain.send_message(request)
 #         response = blockchain.receive_message()
