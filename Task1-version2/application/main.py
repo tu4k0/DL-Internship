2a62106e69382b441db6c3a8bc99dbe501d9d130
tu4k0 <kirya.mlievskij.00@gmail.com>
2023-04-21 17:37:58 +0300

    Refactor main()

--- Task1-version2/application/main.py
+++ Task1-version2/application/main.py
@@ -1,8 +1,5 @@
 from application.application.app import CLI
-from application.bitcoin_blockchain.bitcoin_p2p import BitcoinP2P
-
 
 if __name__ == "__main__":
-    bitcoin = BitcoinP2P()
-    cli = CLI(bitcoin)
+    cli = CLI()
     cli.run()
