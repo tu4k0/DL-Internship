7bf21b8536b180e160a3a81a7a74b15868138a14
tu4k0 <kirya.mlievskij.00@gmail.com>
2023-04-21 19:09:05 +0300

    Add ethereum implementation

--- Task1-version2/application/application/app.py
+++ Task1-version2/application/application/app.py
@@ -1,6 +1,7 @@
 import sys
 
 from application.bitcoin_blockchain.bitcoin_controller import BitcoinController
+from application.ethereum_blockchain.ethereum_controller import EthereumController
 from application.bitcoin_blockchain.bitcoin_config import *
 from application.ethereum_blockchain.eth_config import *
 
@@ -16,11 +17,12 @@ class CLI:
         ip_address = node[0]
         port = int(node[1])
         node_number = int(self.cli_arguments[2])
+        user_request = [ip_address, port, node_number]
         if port == btc_mainnet_port:
-            user_request = [ip_address, port, node_number]
             bitcoin_controller = BitcoinController(user_request)
             bitcoin_controller.start_data_collecting_session()
         elif port == eth_mainnet_port:
-            print("Ethereum")
+            ethereum_controller = EthereumController(user_request)
+            ethereum_controller.start_data_collecting_session()
         else:
             raise Exception("Invalid port")
