f9628ea38024c04f1cc8b0e05a737aadb396424b
tu4k0 <kirya.mlievskij.00@gmail.com>
2023-04-27 17:35:29 +0300

    Refactor controller

--- Task1-version2/application/ethereum_blockchain/ethereum_controller.py
+++ Task1-version2/application/ethereum_blockchain/ethereum_controller.py
@@ -10,12 +10,5 @@ class EthereumController:
         self.ethereum_service = EthereumService(user_request)
 
     def start_data_collecting_session(self):
-        self.ethereum_service.start_session(self.user_request)
-        self.print_blockchain_info()
-
-    def print_blockchain_info(self):
-        print("last block:\t", self.ethereum_service.ethereum.best_block_height, "\thash: ", self.ethereum_service.ethereum.best_block_hash, "nodes: ", self.ethereum_service.ethereum.node_number)
-        print("previous block:\t", self.ethereum_service.ethereum.previous_block_height, "\thash: ",  self.ethereum_service.ethereum.previous_block_hash, "nodes: ", self.ethereum_service.ethereum.node_number)
-        print("total number of sent messages:\t\t",  self.ethereum_service.ethereum.amount_sent_messages)
-        print("total number of received messages:\t",  self.ethereum_service.ethereum.amount_received_messages)
+        self.ethereum_service.start_session()
 
