d39b8a8e67fa5c2529850e2f0b1258db34983e8b
tu4k0 <kirya.mlievskij.00@gmail.com>
2023-04-27 17:36:28 +0300

    Update service (add multithreading)

--- Task1-version2/application/ethereum_blockchain/ethereum_service.py
+++ Task1-version2/application/ethereum_blockchain/ethereum_service.py
@@ -1,81 +1,74 @@
+import csv
 import json
+import sys
 
 from application.ethereum_blockchain.ethereum import Ethereum
 from application.ethereum_blockchain.ethereum_p2p import EthereumP2P
+from application.ethereum_blockchain.ethereum_node_thread import EthereumNodeThread
+
+nodes_list = 'C:/Users/Admin/Desktop/Tu4k0/DL-Internship/Task1-version2/application/ethereum_blockchain/ethereum-nodestrackerlist.csv'
 
 
 class EthereumService:
+    ip: str
+    port: int
+    node_number: int
     ethereum: Ethereum
     ethereum_p2p: EthereumP2P
 
     def __init__(self, user_request):
-        self.ethereum = Ethereum(ip_address=user_request[0], port=user_request[1], node_number=user_request[2])
+        self.ethereum = Ethereum()
         self.ethereum_p2p = EthereumP2P()
+        self.ip = user_request[0]
+        self.port = user_request[1]
+        self.node_number = user_request[2]
 
-    def start_session(self, user_request):
-        ip_address = user_request[0]
-        port = user_request[1]
-        node_number = user_request[2]
-        self.ethereum_p2p.set_socket()
-        connection = self.ethereum_p2p.connect_node(ip_address, port)
-        if connection:
-            print("active connections: ", node_number)
-            best_block_number_payload = self.ethereum_p2p.create_best_block_height_payload()
-            best_block_number_message = self.ethereum_p2p.make_message(best_block_number_payload)
-            self.ethereum_p2p.send_message(best_block_number_message)
-            best_block_number_response = self.ethereum_p2p.receive_message()
-            best_block_number = self.get_best_block_number(best_block_number_response)
-            best_block_hash_payload = self.ethereum_p2p.create_getblock_by_number_payload(best_block_number)
-            best_block_hash_message = self.ethereum_p2p.make_message(best_block_hash_payload)
-            self.ethereum_p2p.send_message(best_block_hash_message)
-            best_block_response = self.ethereum_p2p.receive_message()
-            self.ethereum.best_block_height = best_block_number
-            self.ethereum.best_block_hash = self.get_best_block_hash(best_block_response)
-            self.ethereum.previous_block_height = best_block_number - 1
-            self.ethereum.previous_block_hash = self.get_previous_block_hash(best_block_response)
-            self.ethereum.amount_sent_messages = self.ethereum_p2p.requests
-            self.ethereum.amount_received_messages = self.ethereum_p2p.responses
-
-    def decode_response_message(self, response):
-        message = {}
-        response = response.decode('utf-8')
-        if not response:
-            message.update(result=None)
-        else:
-            status = message['status'] = response[9:15]
-            type = message['type'] = response[43:47]
-            length = message.update(length=len(response))
-            body_start = response.find('{')
-            if 'transactions' in response:
-                body_end = response.rfind('transactions')
-                response = response[body_start:body_end - 2] + '}' + '}'
-                message.update(result=json.loads(response)['result'])
-            else:
-                if response.rfind('parentHash') != -1:
-                    body_end = response.rfind('parentHash') + 80
-                    response = response[body_start:body_end] + '}' + '}'
-                    message.update(result=json.loads(response)['result'])
+    def get_nodes(self):
+        found_peers = dict()
+        found_peers.update({self.ip: self.port})
+        search_index = 0
+        with open(nodes_list, 'r') as nodes:
+            nodes = csv.reader(nodes)
+            for node_info in nodes:
+                if search_index == self.node_number:
+                    break
                 else:
-                    body_end = response.rfind('}')
-                    message.update(result=json.loads(response[body_start:body_end + 1])['result'])
+                    if search_index == 0:
+                        search_index += 1
+                    else:
+                        found_peers.update({node_info[2]: 8545})
+                        search_index += 1
 
-            return message
+        return found_peers
 
-    def get_best_block_hash(self, response):
-        best_block_hash = self.decode_response_message(response)['result']['hash']
+    def start_session(self):
+        node_threads = []
+        nodes = self.get_nodes()
+        while True:
+            for ip, port in nodes.items():
+                node_thread = EthereumNodeThread(ip, port, self.ethereum, self.ethereum_p2p)
+                node_thread.start()
+                node_threads.append(node_thread)
+            # for node in node_threads:
+                node_thread.join()
+            self.print_blockchain_info()
+            self.clear_statistic()
 
-        return best_block_hash
+    def print_blockchain_info(self):
+        print("active connections:\t", self.ethereum.active_connections)
+        print("last block:\t", self.ethereum.best_block_numbers[0], "\thash: ",
+              self.ethereum.best_block_hashes[0], "nodes: ", self.ethereum.best_block_hashes.count(self.ethereum.best_block_hashes[0]))
+        print("previous block:\t", self.ethereum.prev_block_numbers[0], "\thash: ",
+              self.ethereum.prev_block_hashes[0], "nodes: ", self.ethereum.prev_block_hashes.count(self.ethereum.prev_block_hashes[0]))
+        print("total number of sent messages:\t\t", self.ethereum.amount_sent_messages)
+        print("total number of received messages:\t", self.ethereum.amount_received_messages)
 
-    def get_best_block_number(self, response):
-        best_block_number = self.decode_response_message(response)['result']
-        best_block_number = int(best_block_number, 16)
-
-        return best_block_number
-
-    def get_previous_block_hash(self, response):
-        previous_block_hash = self.decode_response_message(response)['result']['parentHash']
-
-        return previous_block_hash
+    def clear_statistic(self):
+        self.ethereum.best_block_numbers.clear()
+        self.ethereum.best_block_hashes.clear()
+        self.ethereum.prev_block_numbers.clear()
+        self.ethereum.prev_block_hashes.clear()
+        self.ethereum.active_connections = 0
 
     def close_session(self):
-        self.ethereum_p2p.disconnect_node()
+        sys.exit('0')
