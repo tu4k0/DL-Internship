c2e1ec92e269aaa6d3382d412fe60ebe0689ad99
tu4k0 <kirya.mlievskij.00@gmail.com>
2023-05-02 14:00:44 +0300

    Create bitcoin node thread class

---
+++ Task1-version2/application/bitcoin_blockchain/bitcoin_node_thread.py
@@ -0,0 +1,10 @@
+import json
+import threading
+
+from application.bitcoin_blockchain.bitcoin import Bitcoin
+from application.bitcoin_blockchain.bitcoin_p2p import BitcoinP2P
+from application.multithreading.base_thread import BaseThread
+
+
+class BitcoinNodeThread(BaseThread):
+    pass
