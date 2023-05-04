b42d0c4e601b8d2e8449cbfdb66b6e55bf33585f
tu4k0 <kirya.mlievskij.00@gmail.com>
2023-05-04 19:38:18 +0300

    Refactor config (naming)

---
+++ Task1-version2/application/ethereum_blockchain/ethereum_config.py
@@ -0,0 +1,9 @@
+ETHEREUM_MAINNET_PORT = 8545
+
+HOST = "192.168.0.100"
+
+JSON_VERSION = '2.0'
+
+JSON_ID = '0'
+
+NODES_LIST_CSV = 'C:/Users/Admin/Desktop/Tu4k0/DL-Internship/Task1-version2/application/ethereum_blockchain/ethereum-nodestrackerlist.csv'
