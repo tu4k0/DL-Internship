de6f8059db8fde7cc918b72dde812c138354b7a0
tu4k0 <kirya.mlievskij.00@gmail.com>
2023-05-04 19:40:58 +0300

    Refactor Ethereum P2P class

--- Task1-version2/application/ethereum_blockchain/ethereum_p2p.py
+++ Task1-version2/application/ethereum_blockchain/ethereum_p2p.py
@@ -1,9 +1,8 @@
-import csv
-import socket
 import json
+import socket
 
 from application.base_blockchain.base_blockchain import BaseBlockchain
-from application.ethereum_blockchain.eth_config import *
+from application.ethereum_blockchain.ethereum_config import *
 
 
 class EthereumP2P(BaseBlockchain):
@@ -17,186 +16,169 @@ class EthereumP2P(BaseBlockchain):
 
     def set_node(self):
         super().set_node()
-        self.node.bind(('', eth_mainnet_port))
+        self.node.bind(('', ETHEREUM_MAINNET_PORT))
 
         return self.node
 
-    def get_nodes(self, nodes_list, node_number):
-        with open(nodes_list, 'r') as nodes:
-            nodes = csv.reader(nodes)
-            found_peers = dict()
-            search_index = 0
-            for node_info in nodes:
-                if search_index == node_number + 1:
-                    break
-                else:
-                    if search_index == 0:
-                        search_index += 1
-                    else:
-                        found_peers.update({node_info[2]: 8545})
-                        search_index += 1
-
-        return found_peers
-
-    def create_message(self, payload):
-        request_method = "POST / HTTP/1.1\r\n"
-        host = f"Host: {host_ip}\r\n"
+    def create_message(self, payload) -> bytes:
+        request_method = f"POST / HTTP/1.1\r\n"
+        host = f"Host: {HOST}\r\n"
         content_type = f"Content-Type: application/json\r\n"
         content_length = f"Content-Length: {len(json.dumps(payload))}\r\n\r\n{json.dumps(payload)}"
-        message = request_method + host + content_type + content_length
+        message = (request_method + host + content_type + content_length).encode('utf-8')
 
-        return message.encode('utf-8')
+        return message
 
-    def create_getdata_payload(self, tx_id):
+    def create_getdata_payload(self, tx_id) -> dict[str, str | list]:
         command = 'eth_getTransactionByHash'
         tx_getdata_message = {
-            'jsonrpc': json_version,
+            'jsonrpc': JSON_VERSION,
             'method': command,
             'params': [tx_id],
-            'id': json_id
+            'id': JSON_ID
         }
 
         return tx_getdata_message
 
-    def create_getblock_by_number_payload(self, block_number):
+    def create_getblock_by_number_payload(self, block_number) -> dict[str, str | list]:
         command = 'eth_getBlockByNumber'
         block_number_message = {
-            'jsonrpc': json_version,
+            'jsonrpc': JSON_VERSION,
             'method': command,
-            'params': [hex(block_number), False],
-            'id': json_id
+            'params': [hex(int(block_number)), False],
+            'id': JSON_ID
         }
 
         return block_number_message
 
-    def create_getblock_by_hash_payload(self, block_hash):
+    def create_getblock_by_hash_payload(self, block_hash) -> dict[str, str | list]:
         command = 'eth_getBlockByHash'
         block_hash_message = {
-            'jsonrpc': json_version,
+            'jsonrpc': JSON_VERSION,
             'method': command,
             'params': [block_hash, False],
-            'id': json_id
+            'id': JSON_ID
         }
 
         return block_hash_message
 
-    def create_getblock_tx_number_payload(self, block_number):
+    def create_getblock_tx_number_payload(self, block_number) -> dict[str, str | list]:
         command = 'eth_getBlockTransactionCountByNumber'
         block_tx_number_message = {
-            'jsonrpc': json_version,
+            'jsonrpc': JSON_VERSION,
             'method': command,
             'params': [hex(block_number)],
-            'id': json_id
+            'id': JSON_ID
         }
 
         return block_tx_number_message
 
-    def create_get_tx_by_hash_payload(self, tx_hash):
+    def create_get_tx_by_hash_payload(self, tx_hash) -> dict[str, str | list]:
         command = 'eth_getTransactionByHash'
         tx_hash_message = {
-            'jsonrpc': json_version,
+            'jsonrpc': JSON_VERSION,
             'method': command,
             'params': [tx_hash],
-            'id': json_id
+            'id': JSON_ID
         }
 
         return tx_hash_message
 
-    def create_best_block_height_payload(self):
+    def create_best_block_height_payload(self) -> dict[str, str | list]:
         command = 'eth_blockNumber'
         best_block_height_message = {
-            'jsonrpc': json_version,
+            'jsonrpc': JSON_VERSION,
             'method': command,
             'params': [],
-            'id': json_id,
+            'id': JSON_ID,
         }
 
         return best_block_height_message
 
-    def create_getnetwork_payload(self):
+    def create_getnetwork_payload(self) -> dict[str, str | list]:
         command = 'eth_chainId'
         network_message = {
-            "jsonrpc": json_version,
+            "jsonrpc": JSON_VERSION,
             "method": command,
             "params": [],
-            "id": json_id
+            "id": JSON_ID
         }
 
         return network_message
 
-    def create_getmining_payload(self):
+    def create_getmining_payload(self) -> dict[str, str | list]:
         command = 'eth_mining'
         mining_message = {
-            "jsonrpc": json_version,
+            "jsonrpc": JSON_VERSION,
             "method": command,
             "params": [],
-            "id": json_id
+            "id": JSON_ID
         }
 
         return mining_message
 
-    def create_getbadblocks_payload(self):
+    def create_getbadblocks_payload(self) -> dict[str, str | list]:
         command = 'debug_getBadBlocks'
         debug_bad_blocks_message = {
-            "jsonrpc": json_version,
+            "jsonrpc": JSON_VERSION,
             "method": command,
             "params": [],
-            "id": json_id
+            "id": JSON_ID
         }
 
         return debug_bad_blocks_message
 
-    def create_getgasprice_payload(self):
+    def create_getgasprice_payload(self) -> dict[str, str | list]:
         command = 'eth_gasPrice'
         gas_price_message = {
-            "jsonrpc": json_version,
+            "jsonrpc": JSON_VERSION,
             "method": command,
             "params": [],
-            "id": json_id
+            "id": JSON_ID
         }
 
         return gas_price_message
 
-    def create_ping_payload(self):
+    def create_syncing_payload(self) -> dict[str, str | list]:
         command = 'eth_syncing'
         ping_message = {
-            'jsonrpc': json_version,
+            'jsonrpc': JSON_VERSION,
             'method': command,
             'params': [],
-            'id': json_id
+            'id': JSON_ID
         }
 
         return ping_message
 
-    def create_hashrate_payload(self):
+    def create_hashrate_payload(self) -> dict[str, str | list]:
         command = 'eth_hashrate'
         hashrate_message = {
-            'jsonrpc': json_version,
+            'jsonrpc': JSON_VERSION,
             'method': command,
             'params': [],
-            'id': json_id
+            'id': JSON_ID
         }
 
         return hashrate_message
 
-    def create_net_listening_payload(self):
+    def create_ping_payload(self) -> dict[str, str | list]:
         command = 'net_listening'
         net_listening_message = {
-            'jsonrpc': json_version,
+            'jsonrpc': JSON_VERSION,
             'method': command,
             'params': [],
-            'id': json_id
+            'id': JSON_ID
         }
 
         return net_listening_message
 
-    def create_net_peer_count_payload(self):
+    def create_net_peer_count_payload(self) -> dict[str, str | list]:
         command = 'net_peerCount'
         net_peer_count_message = {
-            'jsonrpc': json_version,
+            'jsonrpc': JSON_VERSION,
             'method': command,
             'params': [],
-            'id': json_id
+            'id': JSON_ID
         }
 
         return net_peer_count_message
