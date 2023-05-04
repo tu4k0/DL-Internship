ba873741cea64eea5e86a3d43effb70182825019
tu4k0 <kirya.mlievskij.00@gmail.com>
2023-05-04 19:37:24 +0300

    Add methods to base thread (set_thread_name, get_active_connections)

--- Task1-version2/application/multithreading/base_thread.py
+++ Task1-version2/application/multithreading/base_thread.py
@@ -1,9 +1,9 @@
 import threading
 
-from abc import abstractmethod
+from abc import abstractmethod, ABC
 
 
-class BaseThread(threading.Thread):
+class BaseThread(threading.Thread, ABC):
 
     def __init__(self):
         super().__init__()
@@ -12,16 +12,14 @@ class BaseThread(threading.Thread):
     def run(self):
         pass
 
-    @abstractmethod
-    def clear_statistic(self):
-        pass
-
     def stop(self):
         self.join()
+
+    def set_thread_name(self, name):
+        self.name = name
 
     def get_thread_status(self):
         return self.is_alive()
 
-
-
-
+    def get_active_threads(self):
+        return threading.active_count()
