0dbe0e758379f86a28c4c32f38f859e6397043e6
tu4k0 <kirya.mlievskij.00@gmail.com>
2023-05-04 19:42:50 +0300

    Refactor CLI

--- Task1-version2/application/application/app.py
+++ Task1-version2/application/application/app.py
@@ -3,7 +3,7 @@ import sys
 from application.bitcoin_blockchain.bitcoin_controller import BitcoinController
 from application.ethereum_blockchain.ethereum_controller import EthereumController
 from application.bitcoin_blockchain.bitcoin_config import *
-from application.ethereum_blockchain.eth_config import *
+from application.ethereum_blockchain.ethereum_config import *
 
 
 class CLI:
@@ -18,10 +18,10 @@ class CLI:
         port = int(node[1])
         node_number = int(self.cli_arguments[2])
         user_request = [ip_address, port, node_number]
-        if port == btc_mainnet_port:
+        if port == BITCOIN_MAINNET_PORT:
             bitcoin_controller = BitcoinController(user_request)
             bitcoin_controller.start_data_collecting_session()
-        elif port == eth_mainnet_port:
+        elif port == ETHEREUM_MAINNET_PORT:
             ethereum_controller = EthereumController(user_request)
             ethereum_controller.start_data_collecting_session()
         else:
