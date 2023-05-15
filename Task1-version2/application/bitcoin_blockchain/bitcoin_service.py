a9c36b1debd5228d67b6b8fb98df6ff094fc1e1f
tu4k0 <kirya.mlievskij.00@gmail.com>
2023-05-15 14:22:41 +0300

    Update bitcoin service

--- Task1-version2/application/bitcoin_blockchain/bitcoin_service.py
+++ Task1-version2/application/bitcoin_blockchain/bitcoin_service.py
@@ -7,6 +7,7 @@ import ipaddress
 
 from application.bitcoin_blockchain.bitcoin_config import DNS_SEEDS, BITCOIN_GETADDR_COMMAND_HEX
 from application.bitcoin_blockchain.bitcoin import Bitcoin
+from application.bitcoin_blockchain.bitcoin_node import BitcoinNode
 from application.bitcoin_blockchain.bitcoin_p2p import BitcoinP2P
 from application.bitcoin_blockchain.bitcoin_node_thread import BitcoinNodeThread
 from application.bitcoin_blockchain.bitcoin_statistic import BitcoinStatistic
@@ -26,37 +27,46 @@ class BitcoinService:
         self.port = user_request[1]
         self.node_number = user_request[2]
 
-    def start_session(self, user_request):
-        ip_address = user_request[0]
-        port = user_request[1]
-        node_number = user_request[2]
-        found_peers = {}
-        version_payload = self.bitcoin_p2p.create_version_payload(ip_address)
-        version_message = self.bitcoin_p2p.create_message('version', version_payload)
-        verack_message = self.bitcoin_p2p.create_verack_payload()
-        getaddr_payload = self.bitcoin_p2p.create_getaddr_payload()
-        getaddr_message = self.bitcoin_p2p.create_message('getaddr', getaddr_payload)
+    def start_session(self):
+        bitcoin_light_node = BitcoinNode()
+        bitcoin_light_node.connect()
         node = self.bitcoin_p2p.set_socket()
-        connection = self.bitcoin_p2p.connect_node(node, ip_address, port)
+        connection = self.bitcoin_p2p.connect(node, self.ip, self.port)
         if connection:
+            version_payload = self.bitcoin_p2p.create_version_payload(self.ip)
+            version_message = self.bitcoin_p2p.create_message('version', version_payload)
             self.bitcoin_p2p.send_message(node, version_message)
             version_response = self.bitcoin_p2p.receive_message(node)
             if not version_response:
                 sys.exit('Node not responding')
+            bitcoin_light_node.send(version_response)
+            verack_message = self.bitcoin_p2p.create_verack_payload()
+            getaddr_payload = self.bitcoin_p2p.create_getaddr_payload()
+            getaddr_message = self.bitcoin_p2p.create_message('getaddr', getaddr_payload)
             self.bitcoin_p2p.send_message(node, verack_message)
             response_data = self.bitcoin_p2p.receive_message(node)
+            bitcoin_light_node.send(response_data)
             self.bitcoin_p2p.send_message(node, getaddr_message)
-            while True:
+            while 1:
                 if str(response_data).find('addr') != -1:
-                    found_peers = self.get_nodes_from_getaddr(node, response_data, node_number, ip_address, port)
+                    getaddr = binascii.hexlify(response_data)
+                    getaddr_index = str(getaddr).find(BITCOIN_GETADDR_COMMAND_HEX)
+                    if len(getaddr[getaddr_index - 2:]) == 40:
+                        response_data += self.bitcoin_p2p.receive_message(node)
+                        bitcoin_light_node.send(response_data)
+                    found_peers = self.get_nodes_from_getaddr(response_data, self.node_number, self.ip, self.port)
                     break
                 else:
                     response_data = self.bitcoin_p2p.receive_message(node)
+                    bitcoin_light_node.send(response_data)
             node.close()
-        while True:
+        else:
+            sys.exit('Unable to create socket')
+        while 1:
+            start_time = time.perf_counter()
             node_threads = []
             for ip, port in found_peers.items():
-                node = BitcoinNodeThread(ip, port, self.bitcoin, self.bitcoin_p2p)
+                node = BitcoinNodeThread(ip, port, self.bitcoin, self.bitcoin_p2p, bitcoin_light_node)
                 node.start()
                 node_threads.append(node)
             for node in node_threads:
@@ -65,17 +75,16 @@ class BitcoinService:
             bitcoin_statistic.set_amount_sent_messages()
             bitcoin_statistic.set_amount_received_messages()
             bitcoin_statistic.print_blockchain_info()
-            time.sleep(5)
+            avg_processing_time = time.perf_counter() - start_time
+            if avg_processing_time > 5:
+                time.sleep(5)
+            time.sleep(5 - avg_processing_time)
             bitcoin_statistic.clean_statistics()
             bitcoin_statistic.clear_statistic()
 
-    def get_nodes_from_getaddr(self, node, response_data, node_number, ip_address, port):
+    def get_nodes_from_getaddr(self, response_data, node_number, ip_address, port):
         found_nodes = {}
         found_nodes.update({str(ip_address): port})
-        getaddr = binascii.hexlify(response_data)
-        getaddr_index = str(getaddr).find(BITCOIN_GETADDR_COMMAND_HEX)
-        if len(getaddr[getaddr_index - 2:]) == 40:
-            response_data += self.bitcoin_p2p.receive_message(node)
         node_info_size = 12
         response_data = response_data[27:]
         while len(found_nodes) < node_number:
