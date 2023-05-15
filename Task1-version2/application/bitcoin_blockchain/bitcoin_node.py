8fcad71fbcd6e9c3fadf7095f0d43d7f3af15d17
tu4k0 <kirya.mlievskij.00@gmail.com>
2023-05-15 14:21:23 +0300

    Refactor node

--- Task1-version2/application/bitcoin_blockchain/bitcoin_node.py
+++ Task1-version2/application/bitcoin_blockchain/bitcoin_node.py
@@ -1,15 +1,16 @@
 from application.base_blockchain.base_node import BaseNode
+from application.bitcoin_blockchain.bitcoin_config import BITCOIN_MAINNET_PORT
 
 
 class BitcoinNode(BaseNode):
 
-    def __init__(self, ip, port):
+    def __init__(self):
         super().__init__()
-        self.ip = ip
-        self.port = port
+        self.ip = '127.0.0.1'
+        self.port = BITCOIN_MAINNET_PORT
 
     def set_node(self):
         self.node.bind((self.ip, self.port))
 
-    def print_messages(self, data):
+    def present_messages(self, data):
         print(data)
